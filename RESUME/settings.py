b80c36686d4d0ec9f5cc2b1100b110c4a2cb37d4
mykie88 <mykhelmyers@gmail.com>
2021-08-20 01:37:51 +0100

    updated env vars and gitignore

--- RESUME/settings.py
+++ RESUME/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fd&q#xa8_maf!!nwbac&m=(z3!bza9%hgj_q%inzl$g0jewhc0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
