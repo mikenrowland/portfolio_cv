fc6c393372e7f7abc2e182dadc29bb99a8c94bb0
mykie88 <mykhelmyers@gmail.com>
2021-08-20 14:06:53 +0100

    added stmp

--- mySite/views.py
+++ mySite/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import redirect, render
 from .forms import ContactForm
 from django.http import HttpResponse
+from django.core.mail import send_mail, BadHeaderError
 from django.contrib import messages
+from django.conf import settings
 
 # Create your views here.
 
@@ -10,6 +12,13 @@ def home(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             name = request.POST.get('name')
+            email = request.POST.get('email')
+            message = request.POST.get('message')
+            subject = "New Contact Message"
+            try:
+                send_mail(subject, message, email, [settings.EMAIL_RECIPIENT], fail_silently=False)
+            except:
+                return HttpResponse('Invalid header found.')
             form.save()
             messages.info(request, f"Thanks {name} for your message, I will get back to you shortly")
             return redirect('home')
